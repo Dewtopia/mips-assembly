277b82dd5ee277ac1c18be7f6fc3419ba0e2f898
Dewtopia <55421761+Dewtopia@users.noreply.github.com>
2019-09-27 00:33:51 -0700

    Update and rename p3codeBase.s to DEWp3codeBase.s

--- assignment/3-conditional/DEWp3codeBase.s
+++ assignment/3-conditional/DEWp3codeBase.s
@@ -10,8 +10,48 @@
 # b --> $s1
 # c --> $s2
 # d --> $s3
-main:    
-    
+main:  
+
+li $s0, 5
+li $s1, 6
+li $s2, 7
+li $s3, 0
+li $t0, 10
+
+sub $s3, $s3, 1
+
+bge $s0, $t0, function  #go to function
+    addi $s0, 1
+	j L1                #Jumps to L1 if executed, otherwise it would go to function.
+function:
+    sub $s0, $s0, 1
+
+L1:
+add $s3, $s0, $s2
+add $s2, $s0, $s3
+
+bge $s1, $t0, functionTwo
+    addi $s1, 1
+    sub $s2, $s2, 1
+	j L2
+functionTwo:
+    sub $s1, $s1, 1
+    addi $s2, 1
+
+L2:              
+add $s0, $s2, $s1
+add $s1, $s2, $s3
+
+bge $s1, $s2, else		#&& statement, should go to else
+ble $s1, $s0, else
+    add $s3, $s0, $s1
+	j L3
+else:
+    ble $s1, $s2, in_if		#|| statement, should execute
+    bge $s2, $s0, exit
+in_if: 
+    add $s3, $s1, $s2
+
 
 exit:
     la   $a0, albl      # puts albl into arg0 (a0 register) for cout
